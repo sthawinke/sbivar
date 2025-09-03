5785639d0711c8e73d67b8fdaecad7f2d7e44185
stijn <stijn.hawinkel@psb.ugent.be>
2025-09-03 12:11:59 +0200

    Testing the array products

--- R/arrayProd.R
+++ R/arrayProd.R
@@ -3,36 +3,52 @@
 #' @param A An mxmxp array
 #' @param M An mxm matrix
 #' @return An mxmxp array
-#' @description Array resulting from all matrix products of slices of array A mxmxp, with matrix M is mxm
+#' @description Array resulting from all matrix products of slices of array A mxmxp, with matrix M is mxm.
+#' This is a faster version of vapply(seq_len(p), FUN.VALUE = mat, function(i){mat %*% arr[,,i]}),
+#' although it may consume more memory
+#' @details The speedup comes from a single call to %*%, very efficient in BLAS.
 arrayMatProd = function(A, M){
     n = dim(A)[1];p = dim(A)[3]
     dn = dimnames(A)
     # # Reshape A into n x (n*p)
     A <- matrix(A, n, n * p)
     # Multiply in one go
-    result <- M %*% A
+    C <- M %*% A
     # Reshape back to n x n x p
-    result <- array(result, dim = c(n, n, p), dimnames = dn)
-    return(result)
+    C <- array(C, dim = c(n, n, p), dimnames = dn)
+    return(C)
 }
-#' Multiply two arrays of same dimensions along the first two dimensions
+#' Find traces of all inner products of matrices composing arrays A and B, after transposing the second yielding a pxp matrix
 #'
 #' @param A,B mxmxp arrays
-#' @return A pxp matrix
-#' @description Array resulting from all matrix products of slices of array A mxmxp
-#' with those of array B of the same dimensions,
+#' @return A pxp matrix of traces
+#' @description Returns the matrix resulting from the traces of all matrix products of slices of array A mxmxp
+#' with those of array B of the same dimensions. It is a faster version of
+#' vapply(seq_len(p), FUN.VALUE = double(p), function(i){
+#'      vapply(seq_len(p), FUN.VALUE = double(1), function(j){
+#'          tr(tcrossprod(arr[,,i], arr2[,,j]))
+#'      })
+#' })
 #'
-arrayProd = function(A, B){
-    n = dim(A)[1];p = dim(A)[3];k = dim(B)[3]
+#' @details The speedup comes from a single call to crossprod, very efficient in BLAS.
+arrayProdTr = function(A, B){
+    n <- dim(A)[1];p = dim(A)[3];k = dim(B)[3]
+    dn <- list(dimnames(A)[[3]], dimnames(B)[[3]])
     # Reshape each into (n^2) x p
-    A_mat <- matrix(A, n^2, p)
-    B_mat <- matrix(B, n^2, k)
+    A <- matrix(A, n^2, p)
+    B <- matrix(B, n^2, k)
     # p x p result: matrix of all slice inner products
-    C <- crossprod(A_mat, B_mat)
-    dimnames(C) = list(dimnames(A)[[3]], dimnames(B)[[3]])
+    C <- crossprod(A, B)
+    dimnames(C) = dn
     C
 }
-#' Find traces of all inner products of matrices composing arrays A and B, yielding a pxp matrix
+#' Find traces of all inner products of matrices composing arrays A and B yielding a pxp matrix
+#' @description
+#' A faster version of vapply(seq_len(p), FUN.VALUE = double(p), function(i){
+#' vapply(seq_len(p), FUN.VALUE = double(1), function(j){
+#'    tr(arr[,,i] %*% arr2[,,j])
+#'  })
+#'})
 #'
 #' @param A,B mxmxp arrays
 #' @return pxp matrix of traces
@@ -41,11 +57,12 @@ arrayProd2tr <- function(A, B) {
     p <- dim(A)[3]
     k <- dim(B)[3]
     n <- dim(A)[1]
+    dn <- list(dimnames(B)[[3]], dimnames(A)[[3]])
     # reshape: each slice becomes a column
-    Amat <- matrix(aperm(A, c(2,1,3)), n*n, p)
-    Bmat <- matrix(B, n*n, k)
+    A <- matrix(aperm(A, c(2,1,3)), n*n, p)
+    B <- matrix(B, n*n, k)
     # big matrix multiplication gives all traces at once
-    result <- t(crossprod(Amat, Bmat)) # k x p
-    dimnames(result) <- list(dimnames(B)[[3]], dimnames(A)[[3]])
-    result
+    C <- t(crossprod(A, B)) # k x p
+    dimnames(C) <- dn
+    C
 }
---
+++ tests/testthat/test-arrayProducts.R
@@ -0,0 +1,24 @@
+context("Unit tests for array products and traces")
+test_that("Array functions yield the same results as using loops", {
+    m = 3;p = 4
+    mat = matrix(rnorm(m^2), m, m)
+    arr = array(rnorm(m^2*p), dim = c(m,m,p))
+    mat2 = matrix(rnorm(m^2), m, m)
+    arr2 = array(rnorm(m^2*p), dim = c(m,m,p))
+    expect_equal(arrayMatProd(arr, mat),
+                     vapply(seq_len(p), FUN.VALUE = mat, function(i){
+                          mat %*% arr[,,i]
+                     }))
+    expect_equal(arrayProdTr(arr, arr2),
+                     vapply(seq_len(p), FUN.VALUE = double(p), function(i){
+                         vapply(seq_len(p), FUN.VALUE = double(1), function(j){
+                            tr(tcrossprod(arr[,,i], arr2[,,j]))
+                         })
+                     }))
+    expect_equal(arrayProd2tr(arr, arr2),
+                 vapply(seq_len(p), FUN.VALUE = double(p), function(i){
+                     vapply(seq_len(p), FUN.VALUE = double(1), function(j){
+                         tr(arr[,,i] %*% arr2[,,j])
+                     })
+                 }))
+})

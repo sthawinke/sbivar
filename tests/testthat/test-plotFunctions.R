10dc7c87d06da3e123d627eaaa6a827042295ee7
stijn <stijn.hawinkel@psb.ugent.be>
2025-10-07 12:14:00 +0200

    with xlsx write in vignette

--- tests/testthat/test-plotFunctions.R
+++ tests/testthat/test-plotFunctions.R
@@ -4,7 +4,7 @@ test_that("Single image plotting function works", {
     expect_is(plotPairSingle( X = X, Y = Y, Cx = Cx, Ey = Ey, features = c("X1", "Y1")), "ggplot")
 })
 test_that("Multi-image plotting function works", {
-    expect_is(plotTopPair(resGams, Xl = Xl, Yl = Yl, Cxl = Cxl, Eyl = Eyl), "ggplot")
-    expect_is(plotTopPair(resGams, Xl, Yl, Cxl, Eyl, parameter = "cofactor"), "ggplot")
+    expect_is(plotTopPair(resGAMsMulti, Xl = Xl, Yl = Yl, Cxl = Cxl, Eyl = Eyl), "ggplot")
+    expect_is(plotTopPair(resGAMsMulti, Xl, Yl, Cxl, Eyl, parameter = "cofactor"), "ggplot")
     expect_is(plotPairMulti(Xl, Yl, Cxl, Eyl, features = c("X1", "Y1")), "ggplot")
 })

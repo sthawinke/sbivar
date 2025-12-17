ab36b0a29acd460c18b6ce4cf07f6a7bcf3fd726
stijn <stijn.hawinkel@psb.ugent.be>
2025-12-17 18:52:34 +0100

    Vignette compiles

--- R/extractResultsMulti.R
+++ R/extractResultsMulti.R
@@ -76,7 +76,7 @@ extractResultsMulti <- function(result, designDf, method = "BH") {
                     c(coefs, "pVal" = CCT(x[, "pVal"]))
                 })
             } else {
-                intRes = ints[[1]]
+                intRes = ints[1,]
                 fixRes = lapply(fixedOut, function(x) x[[1]])
             }
             c(list("Intercept" = intRes), fixRes)

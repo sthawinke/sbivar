6f41349a7f39cfcb8cb60bd18d6ba95884ac37ee
stijn <stijn.hawinkel@psb.ugent.be>
2026-03-04 15:44:06 +0100

    Passing all checks now?

--- R/plotPair.R
+++ R/plotPair.R
@@ -21,11 +21,12 @@
 #' m <- 9e1
 #' p <- 3
 #' k <- 2
-#' X <- matrix(rnorm(n * p), n, p, dimnames = list(NULL, paste0("X", seq_len(p))))
-#' Y <- matrix(rnorm(m * k), m, k, dimnames = list(NULL, paste0("Y", seq_len(k))))
-#' Cx <- matrix(runif(n * 2), n, 2)
-#' Ey <- matrix(runif(m * 2), m, 2)
-#' colnames(Cx) <- colnames(Ey) <- c("x", "y")
+#' X <- matrix(rnorm(n * p), n, p, dimnames =
+#' list(paste0("sampleX", seq_len(n)), paste0("X", seq_len(p))))
+#' Y <- matrix(rnorm(m * k), m, k, dimnames =
+#' list(paste0("sampleY", seq_len(m)), paste0("Y", seq_len(k))))
+#' Cx <- matrix(runif(n * 2), n, 2, dimnames = list(rownames(X), c("x", "y")))
+#' Ey <- matrix(runif(m * 2), m, 2, dimnames = list(rownames(Y), c("x", "y")))
 #' resMoransI <- sbivar(X, Y, Cx, Ey, method = "Moran's I")
 #' # Plot the feature pair with the most significant signal
 #' plotTopPair(resMoransI, X, Y, Cx, Ey)

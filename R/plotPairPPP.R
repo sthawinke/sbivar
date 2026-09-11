62025cbb06eda88872a264f2d83fb6a7881bb00f
stijn <stijn.hawinkel@psb.ugent.be>
2026-09-11 12:43:17 +0200

    Throw out shapes altogether

--- R/plotPairPPP.R
+++ R/plotPairPPP.R
@@ -5,11 +5,14 @@
 #' @param modalityNames Names to be given to the modalities,
 #' appearing in the strip text of the columns. For plotTopPairPPP() and
 #' plotPairSinglePPP(), the feature names are used.
+#' @param sideBySide Boolean indicating the patterns are to be plotted side by side, or on top of each other if FALSE
+#' @param sizeX,sizeY Desired point sizes for corresponding modalities
 #' @seealso \link{plotTopPair}, \link{sbivarSinglePPP}
 #' @return A ggplot object
 #' @order 3
 #' @export
 #' @rdname plotTopPairPPP
+#' @note The point pattern is coloured blue, the value corresponding to 1
 #' @examples
 #' n <- 8e1
 #' m <- 9e1
@@ -24,42 +27,52 @@
 #' library(spatstat.random)
 #' lambda <- 8e1
 #' p <- 3
-#' PPP <- rmpoispp(lambda, types = paste0("gene", seq_len(p)), win = owin(c(0, 1), c(0, 1)))
+#' PPP <- rmpoispp(lambda, types = paste0("X", seq_len(p)), win = owin(c(0, 1), c(0, 1)))
 #' marks(PPP, drop = FALSE) <- data.frame(feature = marks(PPP)) # Make sure marks is a dataframe
-#' plotPairPPP()
 #' resMoransIppp <- sbivar(X = PPP, Y, Ey, method = "Moran's I")
-#' plotTopPairPPP(resMoransIppp, PPP = X, Y = Y, Ey = Ey)
-plotPairSinglePPvec <- function(
-      Cx, y, Ey, sizeX = 0.1, sizeY = 1, shapeX = 15, shapeY = 16,
-      modalityNames = c("Modality X", "Modality Y"), theme = theme_bw(), ...
-) {
+#' plotTopPairPPP(resMoransIppp, X = PPP, Y = Y, Ey = Ey)
+#' # For overlay, do:
+#' plotPairPPP(X = PPP, Y = Y, Ey = Ey, features = c("X1", "Y1"), sideBySide = FALSE)
+plotPairSinglePPvec <- function(Cx, y, Ey, sizeX = 0.005, sizeY = .01, sideBySide = TRUE,
+    modalityNames = c("Modality X", "Modality Y"), theme = theme_bw(), ...) {
     theme_set(theme)
     stopifnot(length(y) == nrow(Ey), ncol(Ey) == 2, ncol(Cx) == 2)
-    plotDfY <- data.frame(
-        "outcome" = scaleZeroOne(y), Ey,
-        "feature" = modalityNames[2]
-    )
-    plotDfX <- data.frame(
-        Cx,
+    plotDfX <- data.frame(row.names = seq_len(nrow(Cx)),
+        Cx, "outcome" = 1, "size" = sizeX,
         "feature" = modalityNames[1]
     )
-    ggplot(data = plotDfY, aes(x = x, y = y, col = outcome)) +
-        geom_point(size = sizeY, shape = shapeY) +
-        scale_colour_gradient(low = "yellow", high = "blue", name = "Outcome") +
+    plotDfY <- data.frame(
+        Ey, "outcome" = scaleZeroOne(y), "size" = sizeY,
+        "feature" = modalityNames[2]
+    )
+    p <- if (sideBySide) {
+        plotDf = rbind(plotDfX, plotDfY)
+        plotDf$feature = factor(plotDf$feature, levels = modalityNames, ordered = TRUE)
+        ggplot(data = plotDf, aes(x = x, y = y, col = outcome, size = size)) +
+            geom_point() +
+            facet_grid(~feature)
+    } else {
+        ggplot(data = plotDfY, aes(x = x, y = y, col = outcome, size = size)) +
+            geom_point() +
+            geom_point(inherit.aes = FALSE, data = plotDfX, aes(x = x, y = y, size = size))
+    }
+    p + scale_colour_gradient(low = "yellow", high = "blue", name = "Outcome") +
         xlab("x coordinate") +
         ylab("y coordinate") +
         coord_fixed() +
-        geom_point(inherit.aes = FALSE, data = plotDfX, aes(x = x, y = y), size = sizeX, shape = shapeX) +
+        guides(size="none") +
         theme(axis.text = element_blank(), axis.ticks = element_blank())
 }
 #' @rdname plotTopPairPPP
 #' @order 2
 #' @export
 #' @importFrom spatstat.geom coords
-plotPairSinglePPP <- function(X, Y, Ey, features, normY, ...) {
+plotPairPPP <- function(X, Y, Ey, features, normY = "none", ...) {
     Y <- normMat(Y, normY)
-    plotPairSinglePPvec(coords(subset.ppp(X, marks(X, drop = FALSE)$feature == features[1])),
-                               Y[, features[2]], Ey = Ey, ...)
+    plotPairSinglePPvec(Cx = coords(subset.ppp(X, marks(X, drop = FALSE)$feature == features[1])),
+        y = Y[, features[2]],
+        Ey = Ey, modalityNames = features, ...
+    )
 }
 #' @export
 #' @order 1
@@ -67,6 +80,8 @@ plotPairSinglePPP <- function(X, Y, Ey, features, normY, ...) {
 #' @importFrom spatstat.geom is.ppp
 plotTopPairPPP <- function(result, X, Y, Ey, topRank = 1, ...) {
     stopifnot(is.numeric(topRank), topRank >= 1, is.ppp(X))
-    plotPairSinglePPP(X, Y = Y, Ey = Ey, normY = result$normY,
-        features = unlist(result$result[topRank, c("Modality_X", "Modality_Y")]), ...)
+    plotPairPPP(X,
+        Y = Y, Ey = Ey, normY = result$normY,
+        features = unlist(result$result[topRank, c("Modality_X", "Modality_Y")]), ...
+    )
 }
